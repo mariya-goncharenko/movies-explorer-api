26b4e22f3aef11d069151b5c31192a61f05c7e9e
Mariya Goncharenko <114483796+mariya-goncharenko@users.noreply.github.com>
2023-06-15 20:46:02 +0300

    Обновление mw для авторизации.

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,25 +5,28 @@ const { SECRET_KEY_DEV } = require('../config');
 
 const { NODE_ENV, SECRET_KEY } = process.env;
 
-module.exports = (req, res, next) => {
+module.exports = (req, _, next) => {
   const { authorization } = req.headers;
+  const bearer = 'Bearer ';
+  const errorMsg = 'Неправильные почта или пароль';
+
   if (!authorization || !authorization.startsWith('Bearer')) {
-    throw new UnauthorizedError('Неправильные почта или пароль!');
+    return next(new UnauthorizedError(`${errorMsg}!`));
   }
 
-  const token = authorization.replace('Bearer ', '');
+  const token = authorization.replace(bearer, '');
   let payload;
+
   try {
     payload = jwt.verify(
       token,
       NODE_ENV === 'production' ? SECRET_KEY : SECRET_KEY_DEV,
     );
   } catch (err) {
-    next(new UnauthorizedError('Неправильные почта или пароль!'));
-    return;
+    return next(new UnauthorizedError(`${errorMsg}!`));
   }
 
   req.user = payload;
 
-  next();
+  return next();
 };
