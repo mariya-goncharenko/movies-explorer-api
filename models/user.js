f654aec62c569e5b5f061a59a9a9afc19ca5edbc
Mariya Goncharenko <114483796+mariya-goncharenko@users.noreply.github.com>
2023-06-15 17:05:10 +0300

    Добавлен versionKey.

--- models/user.js
+++ models/user.js
@@ -6,43 +6,52 @@ const { Schema } = mongoose;
 
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
-const userSchema = Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    validate: {
-      validator: (v) => isEmail(v),
-      message: 'Неправильный формат почты',
+const userSchema = Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: (v) => isEmail(v),
+        message: 'Неправильный формат почты',
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    name: {
+      type: String,
+      required: true,
+      minlength: 2,
+      maxlength: 30,
     },
   },
-  password: {
-    type: String,
-    required: true,
-    select: false,
+  {
+    versionKey: false,
   },
-  name: {
-    type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 30,
-  },
-});
+);
 
 userSchema.statics.findUserByCredentials = function findOne(email, password) {
-  return this.findOne({ email }).select('+password')
+  return this.findOne({ email })
+    .select('+password')
     .then((user) => {
       if (!user) {
-        return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
+        return Promise.reject(
+          new UnauthorizedError('Неправильные почта или пароль'),
+        );
       }
 
-      return bcrypt.compare(password, user.password)
-        .then((matched) => {
-          if (!matched) {
-            return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
-          }
-          return user;
-        });
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(
+            new UnauthorizedError('Неправильные почта или пароль'),
+          );
+        }
+        return user;
+      });
     });
 };
 
